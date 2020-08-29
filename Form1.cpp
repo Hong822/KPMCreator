4987807f5f78cd0517128c398484bebc37feaff9
kuest <kuest@DESKTOP-MO7LS30>
2020-08-29 12:17:28 +0900

    No Bug

--- Form1.cpp
+++ Form1.cpp
@@ -1,15 +1,14 @@
 #include "pch.h"
 #include "Form1.h"
 //#include <stdafx.h>
-#include <OpenXLSX.hpp>
+//#include <OpenXLSX.hpp>
 
 using namespace CppCLRWinformsProjekt;
 void Form1::Test22()
 {
 	char FilePath[70];// = "D:\\02_KPMTicketCreating\\MIB2_KPM_Ticket_Creator_V8.1.xlsm";
-	lxw_workbook* pWorkbook = 0;
-	pWorkbook = workbook_new(FilePath);
+	/*lxw_workbook* pWorkbook = 0;
+	pWorkbook = workbook_new(FilePath);*/
 	int iii = 100;
 	
-	Test 2;
 }
